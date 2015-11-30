78f3f6caf84146b8aba7b60169d0a96d695262e8
toddbontrager <trbllc@gmail.com>
2015-11-29 20:10:53 -0500

    Update admiss.js

--- admiss.js
+++ admiss.js
@@ -1,1 +1,9 @@
-test
+var myArray = [];
+myArray = ["Todd Bontrager", "toddbontrager"];
+
+var cutName = function()
+
+var myData = {};
+myData.fullName = 
+myData.skype = myArray[1];
+myData.github = "toddbontrager";
