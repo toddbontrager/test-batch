ab12208cd9eac9ba7133c53a912c6e3af18d4b39
toddbontrager <trbllc@gmail.com>
2015-11-30 21:03:18 -0500

    Update admiss.js

--- admiss.js
+++ admiss.js
@@ -1,9 +1,1 @@
-var myArray = ["Todd Bontrager", "toddbontrager"];
-function cutName (str) {
-  return str.split(" ");
-}
-var myData = {
-  fullName: cutName(myArray[0]),
-  skype: myArray[1],
-  github: "toddbontrager"
-};
+//new
