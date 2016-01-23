b2f3ddd23821d605c168ca656d14f36be1b8de4d
Todd Bontager <bontragertodd@gmail.com>
2016-01-22 22:43:46 -0500

    Update ask_about.js

--- ask_about.js
+++ ask_about.js
@@ -2,7 +2,7 @@ var without = function(array, value){
   var results = array.slice();
   var argSource = Array.prototype.slice.call(arguments);
   var args = argSource.slice(1, argSource.length);
-	_.each(results, function(val, index){
+  _.each(results, function(val, index){
     _.each(args, function(valArg){
       if (val === valArg){
         console.log(index);
@@ -21,3 +21,7 @@ var test = without(nums, 1,4,9,10);
 //but was returned [0, 2, 3, 5, 6, 7, 8, 10, 20]
 //when two consecutive values are given as paramaters, the second one does not get removed from the array.
 //I think it has to do with the .splice(index,1) call.
+
+//I think I found the solution:
+//if the results array is larger than the args array, the second _.each function won't iterate through the entire list.
+//to fix this, swap lines 5 & 6.
